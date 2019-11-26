df3ab6f17529a61bce0185bffe59e3d2eaa7adc2
jinpengchao <1093750621@qq.com>
2019-11-26 16:12:06 +0800

    UI_update

--- AndroidProject/vhome/app/src/main/java/h/jpc/vhome/children/fragment/WarnFragment.java
+++ AndroidProject/vhome/app/src/main/java/h/jpc/vhome/children/fragment/WarnFragment.java
@@ -1,21 +1,189 @@
 package h.jpc.vhome.children.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import h.jpc.vhome.R;
+import h.jpc.vhome.children.fragment.dialog.AddNewNormalWarnDialog;
+import h.jpc.vhome.children.fragment.slideadapter.ListViewCompat;
+import h.jpc.vhome.children.fragment.slideadapter.SlideView;
 
-public class WarnFragment extends Fragment {
+public class WarnFragment extends Fragment implements AdapterView.OnItemClickListener, View.OnClickListener, SlideView.OnSlideListener {
+    private static final String TAG = "MainActivity";
+    private ListViewCompat mListView;
+    private List<MessageItem> mMessageItems = new ArrayList<MessageItem>();
+    private SlideView mLastSlideViewWithStatusOn;
+    private SlideAdapter adapter;
+    private AddNewNormalWarnDialog addNewNormalWarnDialog;
+    private Button addNewNormalWarn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_children_warn,null);
+        View view = inflater.inflate(R.layout.fragment_children_warn, null);
+        addNewNormalWarn = view.findViewById(R.id.addNormalWarn);
+        addNewNormalWarn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = getLayoutInflater().inflate(R.layout.add_new_normal_dialog, null);
+                addNewNormalWarnDialog = new AddNewNormalWarnDialog(getActivity(), 0, 0, view, R.style.DialogTheme);
+                addNewNormalWarnDialog.setCancelable(true);
+                addNewNormalWarnDialog.show();
+            }
+        });
+        mListView = view.findViewById(R.id.list);
+
+        for (int i = 0; i < 20; i++) {
+            MessageItem item = new MessageItem();
+            if (i % 3 == 0) {
+                item.iconRes = R.mipmap.chat;
+                item.title = "腾讯新闻";
+                item.msg = "青岛爆炸满月：大量鱼虾死亡";
+                item.time = "晚上18:18"+"--"+i;
+            } else {
+                item.iconRes = R.mipmap.chat;
+                item.title = "微信团队";
+                item.msg = "欢迎你使用微信";
+                item.time = "12月18日";
+            }
+            mMessageItems.add(item);
+        }
+
+        adapter = new SlideAdapter();
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(this);
+
 
         return view;
+    }
+
+
+
+    private class SlideAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+        SlideAdapter() {
+            super();
+            mInflater = getLayoutInflater();
+        }
+
+        @Override
+        public int getCount() {
+            return mMessageItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mMessageItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            SlideView slideView = (SlideView) convertView;
+            if (slideView == null) {
+                View itemView = mInflater.inflate(R.layout.item_listview_delete, null);
+
+                slideView = new SlideView(getActivity());
+                slideView.setContentView(itemView);
+
+                holder = new ViewHolder(slideView);
+                slideView.setTag(holder);
+            } else {
+                holder = (ViewHolder) slideView.getTag();
+            }
+            MessageItem item = mMessageItems.get(position);
+            item.slideView = slideView;
+            item.slideView.shrink();
+
+            holder.icon.setImageResource(item.iconRes);
+            holder.title.setText(item.title);
+            holder.msg.setText(item.msg);
+            holder.time.setText(item.time);
+            holder.deleteHolder.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    mMessageItems.remove(position);
+                    adapter.notifyDataSetChanged();
+                    Toast.makeText(getActivity(), "删除成功",Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            return slideView;
+        }
+
+    }
+
+    public class MessageItem {
+        public int iconRes;
+        public String title;
+        public String msg;
+        public String time;
+        public SlideView slideView;
+    }
+
+    private static class ViewHolder {
+        public ImageView icon;
+        public TextView title;
+        public TextView msg;
+        public TextView time;
+        public ViewGroup deleteHolder;
+
+        ViewHolder(View view) {
+            icon = (ImageView) view.findViewById(R.id.icon);
+            title = (TextView) view.findViewById(R.id.title);
+            msg = (TextView) view.findViewById(R.id.msg);
+            time = (TextView) view.findViewById(R.id.time);
+            deleteHolder = (ViewGroup)view.findViewById(R.id.holder);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+    long id) {
+//            Toast.makeText(getActivity(), "onItemClick position=" + position,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onSlide(View view, int status) {
+        if (mLastSlideViewWithStatusOn != null && mLastSlideViewWithStatusOn != view) {
+            mLastSlideViewWithStatusOn.shrink();
+        }
+
+        if (status == SLIDE_STATUS_ON) {
+            mLastSlideViewWithStatusOn = (SlideView) view;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.holder:
+                break;
+            default:
+                break;
+        }
     }
 }
--- AndroidProject/vhome/app/src/main/java/h/jpc/vhome/parents/fragment/CommunityFragment.java
+++ AndroidProject/vhome/app/src/main/java/h/jpc/vhome/parents/fragment/CommunityFragment.java
@@ -4,18 +4,113 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTabHost;
 import h.jpc.vhome.R;
+import h.jpc.vhome.parents.fragment.fragment.HotspotFragment;
+import h.jpc.vhome.parents.fragment.fragment.BFragment;
+import h.jpc.vhome.parents.fragment.fragment.CFragment;
+import h.jpc.vhome.parents.fragment.fragment.DFragment;
 
 public class CommunityFragment extends Fragment {
+    private Map<String,TextView> textViewMap = new HashMap<>();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_parent_community,null);
-
+        //获取FragmentTabHost对象
+        FragmentTabHost fragmentTabHost = view.findViewById(android.R.id.tabhost);
+        //初始化参数
+        fragmentTabHost.setup(getActivity(),
+                getChildFragmentManager(),
+                android.R.id.tabcontent
+        );
+        //tab1
+        TabHost.TabSpec tabSpec1 = fragmentTabHost
+                .newTabSpec("tag1")
+                .setIndicator(getTabSpaceView("tag1","首页"));
+        fragmentTabHost.addTab(tabSpec1,
+                HotspotFragment.class,
+                null
+        );
+        //tab2
+        TabHost.TabSpec tabSpec2 = fragmentTabHost
+                .newTabSpec("tag2")
+                .setIndicator(getTabSpaceView("tag2","社区"));
+        fragmentTabHost.addTab(tabSpec2,
+                BFragment.class,
+                null
+        );
+        //tab3
+        TabHost.TabSpec tabSpec3 = fragmentTabHost
+                .newTabSpec("tag3")
+                .setIndicator(getTabSpaceView("tag3","子女"));
+        fragmentTabHost.addTab(tabSpec3,
+                CFragment.class,
+                null
+        );
+        //tab4
+        TabHost.TabSpec tabSpec4 = fragmentTabHost
+                .newTabSpec("tag4")
+                .setIndicator(getTabSpaceView("tag4","我"));
+        fragmentTabHost.addTab(tabSpec4,
+                DFragment.class,
+                null
+        );
+        fragmentTabHost.setCurrentTab(0);
+        textViewMap.get("tag1").setTextColor(getResources().getColor(R.color.choseColor));
+        textViewMap.get("tag2").setTextColor(getResources().getColor(R.color.notChoseColor));
+        textViewMap.get("tag3").setTextColor(getResources().getColor(R.color.notChoseColor));
+        textViewMap.get("tag4").setTextColor(getResources().getColor(R.color.notChoseColor));
+        fragmentTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                switch (tabId){
+                    case "tag1":
+                        textViewMap.get("tag1").setTextColor(getResources().getColor(R.color.choseColor));
+                        textViewMap.get("tag2").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag3").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag4").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        break;
+                    case "tag2":
+                        textViewMap.get("tag1").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag2").setTextColor(getResources().getColor(R.color.choseColor));
+                        textViewMap.get("tag3").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag4").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        break;
+                    case "tag3":
+                        textViewMap.get("tag1").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag2").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag3").setTextColor(getResources().getColor(R.color.choseColor));
+                        textViewMap.get("tag4").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        break;
+                    case "tag4":
+                        textViewMap.get("tag1").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag2").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag3").setTextColor(getResources().getColor(R.color.notChoseColor));
+                        textViewMap.get("tag4").setTextColor(getResources().getColor(R.color.choseColor));
+                        break;
+                }
+            }
+        });
+        return view;
+    }
+    public View getTabSpaceView(String tag, String title){
+        //加载布局文件
+        LayoutInflater layoutInflater1 = getLayoutInflater();
+        View view = layoutInflater1.inflate(R.layout.tab_space_parent,null);
+        //Text对象
+        TextView textView = view.findViewById(R.id.tv_title);
+        textView.setText(title);
+        textViewMap.put(tag,textView);
         return view;
     }
 }
