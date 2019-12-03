1075c77af6f680904038cc060c4fe01f096d4da6
jinpengchao <1093750621@qq.com>
2019-12-03 18:18:01 +0800

    clear

---
+++ AndroidProject/vhome/app/src/main/java/h/jpc/vhome/user/SetNewPwdActivity.java
@@ -0,0 +1,15 @@
+package h.jpc.vhome.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+import h.jpc.vhome.R;
+
+import android.os.Bundle;
+
+public class SetNewPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_new_pwd);
+    }
+}
