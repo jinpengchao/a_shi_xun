f56f7c68bd04264158b57f0246522a66a64797e0
wangziv <2605124892@qq.com>
2019-12-07 15:12:27 +0800

    wzw增加收藏功能

--- AndroidProject/vhome/app/src/main/java/h/jpc/vhome/parents/fragment/adapter/HotSpotAdapter.java
+++ AndroidProject/vhome/app/src/main/java/h/jpc/vhome/parents/fragment/adapter/HotSpotAdapter.java
@@ -8,18 +8,28 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import h.jpc.vhome.MyApp;
 import h.jpc.vhome.R;
+import h.jpc.vhome.parents.fragment.community_hotspot.entity.CollectionBean;
 import h.jpc.vhome.parents.fragment.community_hotspot.entity.PostBean;
+import h.jpc.vhome.util.ConnectionUtil;
 
 public class HotSpotAdapter extends BaseAdapter {
     private List<PostBean> list;
@@ -47,7 +57,7 @@ public class HotSpotAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int i, View view, ViewGroup viewGroup) {
         ViewHolder holder = null;
         if(null==view){
             LayoutInflater inflater = LayoutInflater.from(context);
@@ -57,10 +67,14 @@ public class HotSpotAdapter extends BaseAdapter {
             holder.tvHotName = view.findViewById(R.id.tv_hot_name);
             holder.tvHotContent = view.findViewById(R.id.tv_hot_content);
             holder.tvHotTime = view.findViewById(R.id.tv_hot_time);
+            holder.ivHotSave = view.findViewById(R.id.iv_hot_save);
             holder.tvHotComnum = view.findViewById(R.id.tv_hot_comnum);
             holder.ivHotlike = view.findViewById(R.id.iv_hot_like);
             holder.tvHotLikenum = view.findViewById(R.id.tv_hot_likenum);
             holder.gvPostShow = view.findViewById(R.id.gv_post_show);
+            holder.rlPostSave = view.findViewById(R.id.rl_post_save);
+            holder.rlPostComment = view.findViewById(R.id.rl_post_comment);
+            holder.rlPostLike = view.findViewById(R.id.rl_post_like);
             view.setTag(holder);
         }else {
             holder = (ViewHolder) view.getTag();
@@ -89,16 +103,65 @@ public class HotSpotAdapter extends BaseAdapter {
         holder.gvPostShow.setAdapter(showPostImgAdapter);
         holder.tvHotLikenum.setText(list.get(i).getLikeNum()+"");
         holder.tvHotComnum.setText(list.get(i).getCommentNum()+"");
+        final ViewHolder finalHolder = holder;
+        //点击收藏的时候改变图标颜色
+        holder.rlPostSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finalHolder.ivHotSave.setImageResource(R.mipmap.post_save1);
+                addPostCollection(i);
+            }
+        });
         return view;
     }
+
+    private void addPostCollection(int i) {
+        //首先准备收藏的数据
+        CollectionBean collection = new CollectionBean();
+        PostBean post = list.get(i);
+        collection.setPersonId(post.getPersonId());
+        collection.setPostId(post.getId());
+        Date collectDate = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = sdf.format(collectDate);
+        collection.setTime(time);
+        Gson gson = new Gson();
+        final String data = gson.toJson(collection);
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://"+(new MyApp()).getIp()+":8080/vhome/SaveCollectionServlet");
+                    ConnectionUtil util = new ConnectionUtil();
+                    HttpURLConnection connection = util.sendData(url,data);
+                    String receive = util.getData(connection);
+                    if (null!=receive && !"".equals(receive)){
+                        Log.i("hotSpotAdapter","收藏成功"+data);
+                    }else {
+                        Log.e("hotSpotAdapter","收藏失败！"+data);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+    }
+
     static final class ViewHolder{
         ImageView ivHotPerson;
         TextView tvHotName;
         TextView tvHotContent;
         TextView tvHotTime;
+        ImageView ivHotSave;
         TextView tvHotComnum;
         ImageView ivHotlike;
         TextView tvHotLikenum;
         GridView gvPostShow;
+        RelativeLayout rlPostSave;
+        RelativeLayout rlPostComment;
+        RelativeLayout rlPostLike;
     }
 }
