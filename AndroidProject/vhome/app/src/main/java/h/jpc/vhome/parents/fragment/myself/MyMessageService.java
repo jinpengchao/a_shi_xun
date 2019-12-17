b3eefb9a16e07ce473dc3f5b6d0a6a679bb6f6ce
jinpengchao <1093750621@qq.com>
2019-12-17 21:20:22 +0800

    闹钟功能全部完成

---
+++ AndroidProject/vhome/app/src/main/java/h/jpc/vhome/parents/fragment/myself/MyMessageService.java
@@ -0,0 +1,13 @@
+package h.jpc.vhome.parents.fragment.myself;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import cn.jpush.android.service.JCommonService;
+
+public class MyMessageService extends JCommonService {
+    public MyMessageService() {
+    }
+
+}
