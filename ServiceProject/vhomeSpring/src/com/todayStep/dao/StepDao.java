7c723efede8b69831619631d9c01347a1a47e7ba
jixiaosheng <2050869809@qq.com>
2020-05-13 17:21:16 +0800

    spring

---
+++ ServiceProject/vhomeSpring/src/com/todayStep/dao/StepDao.java
@@ -0,0 +1,37 @@
+package com.todayStep.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.persistence.Query;
+
+import org.hibernate.SessionFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Repository;
+
+import com.entity.TodayStepData;
+
+@Repository
+public class StepDao {
+	@Resource
+	private SessionFactory sessionFactory;
+	
+	public long getLatestStep() {
+		String hql="from TodayStepData order by id desc ";
+		Query query = this.sessionFactory.getCurrentSession().createQuery(hql);
+		query.setFirstResult(0);
+		query.setMaxResults(1);
+		List<TodayStepData> steps = query.getResultList();
+		TodayStepData step = steps.get(0);
+		return step.getStep();
+	}
+	
+	public void saveStep(TodayStepData stepData) {
+		this.sessionFactory.getCurrentSession().save(stepData);
+	}
+	@Scheduled(cron="59 59 23 * * ?")
+	public void deleteStep() {
+		Query delete = this.sessionFactory.getCurrentSession().createQuery("delete from TodayStepData where _id>0");
+		delete.executeUpdate();
+	}
+}
