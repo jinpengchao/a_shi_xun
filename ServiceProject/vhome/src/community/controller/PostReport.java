4e76126d0ae0083e4488c36356395f58765fc0b2
FateNight <37129757+FateNighting@users.noreply.github.com>
2020-05-15 19:06:04 +0800

    Create PostReport.java

---
+++ ServiceProject/vhome/src/community/controller/PostReport.java
@@ -0,0 +1,47 @@
+package community.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostReport
+ */
+@WebServlet("/PostReport")
+public class PostReport extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PostReport() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/text;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		//获取前端发送的举报帖子的id
+		String idString=request.getParameter("postId11");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
