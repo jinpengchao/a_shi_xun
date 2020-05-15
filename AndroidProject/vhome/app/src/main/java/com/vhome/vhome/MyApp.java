22927a5c7f22678c44eb1097311dc864f88f73ee
FateNight <37129757+FateNighting@users.noreply.github.com>
2020-05-15 16:17:39 +0800

    新闻部分更新

--- AndroidProject/vhome/app/src/main/java/com/vhome/vhome/MyApp.java
+++ AndroidProject/vhome/app/src/main/java/com/vhome/vhome/MyApp.java
@@ -68,7 +68,7 @@ public class MyApp extends Application {
 
     // 39.96.24.133
     //别tm有空格
-    private String ip = "192.168.0.104";
+    private String ip = "192.168.0.100";
     private String pathInfo = "parentUserInfo";
 
     private static Handler sHandler=null;
--- AndroidProject/vhome/app/src/main/java/com/vhome/vhome/parents/fragment/news/fragment/TopFragment.java
+++ AndroidProject/vhome/app/src/main/java/com/vhome/vhome/parents/fragment/news/fragment/TopFragment.java
@@ -212,6 +212,9 @@ public class TopFragment extends BaseFragment {
     public void refreshData(){
         news.clear();
         load();
+        lvStus= (ListView)getActivity().findViewById(R.id.lv_data);
+        newsAdapter=new NewsAdapter(getActivity(),news);
+        lvStus.setAdapter(newsAdapter);
         newsAdapter.notifyDataSetChanged();
     }
     public void loadMoreData(){
